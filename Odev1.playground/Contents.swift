d61f32ba6f038c295c87835c2964597622775a79
mzyavuz <melikezeynep_97@hotmail.com>
2022-09-22 11:44:53 +0300

    Odev 1 yüklendi

---
+++ Odev1.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+//islem
+var islemNo = 45721580
+var ogrNo: Int
+var kitapNo: Int
+var aTarih = "22 Eylül 2022"
+var vTarih = "1 Ekim 2022"
+
+//ogrenci
+ogrNo = 2022015
+var ogrAd = "Zeynep"
+var ogrSoyad = "Yavuz"
+var cinsiyet = "Kadın"
+var dTarih = "01 Ocak 1997"
+var sinif = "Mezun"
+
+//kitap
+kitapNo = 55
+var isbNo = 141312
+var kitapAdi = "Beyaz Zambaklar Ülkesi"
+var yazarNo: Int
+var turNo = 15
+var sayfaSayisi = 350
+var puan = 100
+
+//yazar
+yazarNo = 2025
+var yazarAd = "İskender"
+var yazarSoyad = "Pala"
+
+//tur
+turNo; //tanımlı
+var turAdi = "Roman"
